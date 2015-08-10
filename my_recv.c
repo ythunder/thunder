a8e6b9c5a0038587fd100082e2bcf20521010799
ythunder <1368282581@qq.com>
2015-08-10 21:08:56 +0800

    从缓存区或套接字读取数据

--- my_recv.c
+++ my_recv.c
@@ -1,48 +1,47 @@
-#define MY_RECV_C
-
-#include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <stdio.h>
 #include <string.h>
 #include <errno.h>
-#include "my_recv.h"
 #include <stdlib.h>
 
-/*自定义错误处理函数*/
-void my_err(const char *err_string, int line) 
+void my_err(const char *err_string, int line)
 {
-    fprintf(stderr,"line:%d ",line);
+    fprintf(stderr,"line:%d ", line);
     perror(err_string);
     exit(1);
 }
 
+/*****************************************
+    conn_fd:从该连接套接字上获取数据
+    data_buf:读取到数据保存到此缓存
+    len :data所指的空间长度
+    实现从套接字上读取一次数据
+*****************************************/
 int my_recv(int conn_fd, char *data_buf, int len)
 {
-    static char	recv_buf[BUFSIZE];			//自定义缓冲区,BUFSIZE定义在my_recv.h中
-    static char *pread;					//指向下一次读取数据的位置
-    static int  len_remain = 0;				//自定义缓冲区中剩余字节数
-    int		i;					
+    static char recv_buf[1024];   //自定义缓存区
+    static char *pread;		  //指向下一次读取数据的位置
+    static int len_remain = 0;    //自定义缓存区中剩余字节数
+    int i;
 
-    /*如果自定义缓冲区中没有数据,则从套接字中读取数据*/
-    if (len_remain <= 0) {
-        if ((len_remain = recv(conn_fd, recv_buf, sizeof(recv_buf), 0)) < 0) { 	//从fd接收数据保存到buf,执行成功返回接收到的数据字节数
-	    my_err("recv" ,__LINE__);
-        } else if (len_remain == 0) {
+    //如果自定义缓存区中没有数据,则从套接字中读取数据
+    if(len_remain <= 0) {
+	if( (len_remain = recv(conn_fd, recv_buf, sizeof(recv_buf), 0)) < 0) {
+	    my_err("recv", __LINE__);
+	} else if(len_remain == 0) {
 	    return 0;
-        }
-        pread = recv_buf;					//重新初始化pread指针,使其指向缓冲区头	
+	}
+	pread = recv_buf;   //重新初始化pread指针
     }
-
-    /*从自定义缓冲区中读取一次数据*/
+    //从自定义缓存取中读取一次数据
     for(i=0; *pread != '\n'; i++) {
-	if(i > len) {						//len为缓冲区data_buf的大小,存入数据时防止指针越界
+	if(i > len) {
 	    return -1;
 	}
-	data_buf[i] = *pread++;					//将自定义缓冲区中的数据转存入data_buf
+	data_buf[i] = *pread++;
 	len_remain--;
     }
-
-    /*去除结束标志*/
     len_remain--;
     pread++;
 
