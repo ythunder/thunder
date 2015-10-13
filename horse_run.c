7dec58c54656d3d5a0915bce2b02df59d899135a
ythunder <1368282581@qq.com>
2015-10-13 11:23:13 +0800

    贪心马踏棋盘

--- horse_run.c
+++ horse_run.c
@@ -10,12 +10,11 @@
 #include <string.h>
 #include <errno.h>
 
-#define N 8 //棋盘大小
+#define N 8//棋盘大小
 #define Stack_SIZE 100  //存储空间初始分配量
 #define STACKINCREMENT 10 //存储空间分配增量
-
-int weight[N][N];	
-
+	
+int weight[N][N];
 typedef struct 
 {
 	int		x;
@@ -163,119 +162,122 @@ PosType Get_Start_Seat()
 	
 }
 
+//计算某点权值
+int Seat_Weight(PosType curpos) 
+{	
+	int i, qw[N][N];
+	PosType next;
+
+	qw[curpos.x][curpos.y] = 0;
+	for(i=0; i<8; i++) {
+		next = NextPos(curpos, i+1);
+		if(weight[next.x][next.y] == 0 && Can_Walk(next) == 1) {
+			qw[curpos.x][curpos.y]++;
+		}
+	}
+	return qw[curpos.x][curpos.y];
+}
+
+int Seat_Min(int *a)
+{
+	int i, min = 9;
+	int di = 9;
+	for(i=0; i<8; i++) {
+		if(a[i] < min && a[i] > 0) {
+			min = a[i];
+			di = i;
+		}
+	}
+	if(di == 9) {
+		return -1;
+	} else {
+	return di; 
+	}
+}
+
 
 int main()
 {
-	LinkStack  *S = NULL;
-	PosType start, next, st;
-	int g, i, j,horsestep = 0, test;
-	ElemType  *p, *ptemp, *top, *a, *b, *c, *d;
-	int step[N][N];
-	int ords = 0; 
-	int flag = 0, flag1=0;
-
+	LinkStack  *S = NULL;  	PosType start, next, st;	int g, i, j,horsestep = 0, test;	ElemType  *p, *ptemp, *top, *a, *b, *c, *d;
+	int step[N][N];	int ords = 0; 	int  ff[8];	int  min_weight;	int  quan[N][N]; int  flag=0 ,off;
 	memset(step, 0, sizeof(step));
 
 	S = (LinkStack *) malloc (sizeof(LinkStack));
-
 	Set_Empty_Stack(S);		//初始化一个空栈
-	
 	start = Get_Start_Seat();   //得到起始点
 
 	p = (ElemType *) malloc (sizeof(ElemType));
-	p->ord = ++ords;
+	p->ord = ++horsestep;
 	p->seat.x = start.x;
 	p->seat.y = start.y;
 	p->di = 0;
-
 	if(Push(S, p) == 0) {								//将初始位置压入栈
 		fprintf(stderr, "line:%d\n", __LINE__);
 	};
 
-	weight[p->seat.x][p->seat.y] = 1;
-	horsestep++;										//马走一步
-
-	do{
-		flag++;
-		if(flag == 1000) {
-			printf("202\n");
-			return 0;
-		}
+	weight[p->seat.x][p->seat.y] = 1;					//weight 表示该点走没走过
+	do{ 
+		flag = 0;
+		memset(ff, 0, sizeof(ff));
 		for(i=0; i<8; i++) {
 			next = NextPos(start, i+1);
-			if((weight[next.x][next.y] == 0) && (Can_Walk(next) == 1)) {
-				step[start.x][start.y] = i+1;   //访问方式，从周边1-8访问，step记录该点被访问到的数
-				break;
-			}
+			if(Can_Walk(next) == 1 && weight[next.x][next.y] == 0) {
+				quan[next.x][next.y] = Seat_Weight(next);
+				ff[i] = quan[next.x][next.y];
+				flag = 1;
+				off = i;
+			}	
 		}
-		if(i == 8) {
-			step[start.x][start.y] = 9;
+		min_weight = Seat_Min(ff);
+		if(horsestep == N*N-1 && flag==1) {
+			next = NextPos(start, i+1);
+		} else {
+			next = NextPos(start, min_weight + 1);
 		}
-
-		while(step[start.x][start.y] > 8) {
-
-			flag1 ++;
-
+		while(min_weight < 0 && flag ==0) {
 			c = (ElemType *) malloc (sizeof(ElemType));
 			if(Pop(S, c) == 0 ) {							//取栈顶a
 				fprintf(stderr, "line:%d\n", __LINE__);
 			}
-			ords--;
-	
-			weight[c->seat.x][c->seat.y] = 0;
 			horsestep--;
+			weight[c->seat.x][c->seat.y] = 0;
 			d = (ElemType *) malloc (sizeof(ElemType));
 			if(Get_Stack_Top(S, d) == 0) {					//得到栈顶元素
 				fprintf(stderr, "line:%d\n", __LINE__);
 			}
-
 			st.x = d -> seat.x;								//栈顶元素
-			st.y = d -> seat.y;
-					
-			g = step[st.x][st.y];
-			if(g == 8) {
-				step[start.x][start.y] = 9;
-			} else {
-			for(i = g; i < 8; i++) {
+			st.y = d -> seat.y;				
+			for(i = 0; i < 8; i++) {
 				next = NextPos(st, i+1);
-				if((weight[next.x][next.y] == 0) && (Can_Walk(next) == 1)) {
-					step[st.x][st.y] = i+1;
-					break;
-				}
-				
+				if(Can_Walk(next) == 1 && weight[next.x][next.y] == 0) {
+				quan[next.x][next.y] = Seat_Weight(next);
+				ff[i] = quan[next.x][next.y];
 			}
-			if(i == 8) {
-			step[start.x][start.y] = 9;
-			} else {
-			step[start.x][start.y] = step[st.x][st.y];
-			}
-			}
+			}	min_weight = Seat_Min(ff);
+					next = NextPos(st, min_weight + 1);
+
 			free(c);free(d);
-		}
+			}
 			ptemp = (ElemType *) malloc (sizeof(ElemType));
-			ptemp -> ord = ++ords;
+			ptemp -> ord = ++horsestep;
 			ptemp -> seat.x = next.x;
 			ptemp -> seat.y = next.y;
-			ptemp -> di = step[start.x][start.y];
+			ptemp -> di = min_weight;
 		
 			if(Push(S, ptemp) == 0) {
 				fprintf(stderr, "line:%d\n", __LINE__);
 			}
-			
 			weight[ptemp->seat.x][ptemp->seat.y] = 1;
-			horsestep++;
 		
+			top = (ElemType *) malloc (sizeof(ElemType));	//为指针top分配空间
+			Get_Stack_Top(S, top);							//取得栈顶值存入top
+			start.x = top->seat.x;
+			start.y = top->seat.y;
 
-		top = (ElemType *) malloc (sizeof(ElemType));	//为指针top分配空间
-		Get_Stack_Top(S, top);							//取得栈顶值存入top
-		start.x = top -> seat.x;
-		start.y = top -> seat.y;
-	
-		free(top);
-		free(ptemp);
-	} while(horsestep < 61);
-
-	memset(weight, 0, sizeof(weight));
+			free(top);
+			free(ptemp);
+	} while(horsestep < N*N);
+	printf("\n");
 	for(i=0; i<horsestep; i++) {
 		a = (ElemType *) malloc (sizeof(ElemType));
 		Pop(S, a);
